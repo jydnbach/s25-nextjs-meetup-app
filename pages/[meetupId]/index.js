81cfa2cf79bb4664b4a711abc15a8a549c69d453
jydnbach <jaydenkim95@gmail.com>
2024-06-27 19:24:38 +0900

    getstaticpaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,41 @@ function MeetupDetails() {
   );
 }
 
+export function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for single meetup
+
+  const { meetupId } = context.params;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/1500_block_of_Corcoran_Street%2C_N.W..JPG/2560px-1500_block_of_Corcoran_Street%2C_N.W..JPG',
+        id: meetupId,
+        title: 'first meetup',
+        address: 'address',
+        description: 'desc',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
