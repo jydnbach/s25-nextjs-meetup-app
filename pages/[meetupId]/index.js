ac54370fd061e2dfe171c5de2db0e37102d21e86
jydnbach <jaydenkim95@gmail.com>
2024-06-27 18:46:49 +0900

    custom component and css

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/1500_block_of_Corcoran_Street%2C_N.W..JPG/2560px-1500_block_of_Corcoran_Street%2C_N.W..JPG"
+      title="first meetup"
+      address="some street"
+      description="first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
