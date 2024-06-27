8247fda4eb70e090b3bef286a374ad47ff66641f
jydnbach <jaydenkim95@gmail.com>
2024-06-27 20:14:25 +0900

    connevcting wmongdb

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  function addMeetupHandler(data) {
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredData) {
+    // url: same as api folder path
+    const res = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredData),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const data = await res.json();
     console.log(data);
+
+    router.push('/');
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
